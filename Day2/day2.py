86eada1db645ec52be87ec76b103ef7c93fa03c9
bondhan <aobond2@gmail.com>
2023-01-27 13:52:46 +0000

    Added stubs

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,7 +1,28 @@
+scoreArray = []
+
 def ParseLine(line):
     line = line.strip()
     handInput = line.split(" ")
-    print(handInput)
+    doCalculation(handInput)
+
+def doCalculation(arrayInput):
+    Me = (arrayInput[0])
+    You = (arrayInput[1])
+    # Draw
+    if (Me == You):
+        draw()
+    # Win
+    # Lose
+
+def win():
+    print ("")
+
+def draw():
+    print ("")
+
+def lost():
+    print ("")
+
 with open("input.txt") as inputFile:
     lines = inputFile.readlines()
     for l in lines:
