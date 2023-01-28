3b94020b5bed6ab2e531347c94a3efa92d2c4bc6
bondhan <aobond2@gmail.com>
2023-01-28 15:10:15 +0000

    Day 2 update

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,4 +1,7 @@
 scoreArray = []
+winPoint = 6
+drawPoint = 3
+losePoint = 0
 
 def ParseLine(line):
     line = line.strip()
@@ -8,22 +11,60 @@ def ParseLine(line):
 def doCalculation(arrayInput):
     Me = (arrayInput[0])
     You = (arrayInput[1])
-    # Draw
-    if (Me == You):
-        draw()
-    # Win
-    # Lose
+    # Rock
+    if (Me == "A"):
+        actionPoint = 1
+        if (You == "X"):
+            draw(actionPoint)
+        elif (You == "Y"):
+            lost(actionPoint)
+        elif (You == "Z"):
+            win(actionPoint)
+    # Paper
+    if (Me == "B"):
+        actionPoint = 2
+        if (You == "X"):
+            win(actionPoint)
+        elif (You == "Y"):
+            draw(actionPoint)
+        elif (You == "Z"):
+            lost(actionPoint)
+    # Scissor
+    if (Me == "C"):
+        actionPoint = 3
+        if (You == "X"):
+            lost(actionPoint)
+        elif (You == "Y"):
+            win(actionPoint)
+        elif (You == "Z"):
+            draw(actionPoint)
 
-def win():
-    print ("")
+def win(ap):
+    score = winPoint + ap
+    scoreArray.append(score)
 
-def draw():
-    print ("")
+def draw(ap):
+    score = drawPoint + ap
+    scoreArray.append(score)
 
-def lost():
-    print ("")
+def lost(ap):
+    score = losePoint + ap
+    scoreArray.append(score)
 
-with open("input.txt") as inputFile:
-    lines = inputFile.readlines()
+def sumA():
+    s = sum(scoreArray)
+    print (s)
+
+def part_one():
+    with open("input.txt") as inputFile:
+        lines = inputFile.readlines()
     for l in lines:
         ParseLine(l)
+
+    sumA()
+
+def main() -> None:
+    part_one()
+
+if __name__ == "__main__":
+    main()
