582ffe90ba32527803cda3b7c365134e471fb7ff
bondhan <aobond2@gmail.com>
2023-01-28 18:22:36 +0000

    Day 3 part 2

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -6,6 +6,7 @@ for index, letter in enumerate(string.ascii_uppercase):
     values[letter] = index + 27
 
 prioritiesSum = 0
+threeElfArray = []
 
 def part_one():
     with open("input.txt") as inputFile:
@@ -14,17 +15,37 @@ def part_one():
         ParseLine(l)
     print (prioritiesSum)
 
+def part_two():
+    global threeElfArray
+    with open("input.txt") as inputFile:
+        lines = inputFile.readlines()
+    for l in lines:
+        if (len(threeElfArray) <= 2):
+            threeElfArray.append(l)
+            checkThreeInput()
+    
+    print(prioritiesSum)
+
+def checkThreeInput():
+    global prioritiesSum
+    global threeElfArray
+    if (len(threeElfArray) == 3):
+        sameLetters = list(set(threeElfArray[0].strip()) & set(threeElfArray[1].strip()) & set(threeElfArray[2].strip()))
+        for l in sameLetters:
+            prioritiesSum += (values[l])
+        threeElfArray.clear()
+
 def ParseLine(line):
     global prioritiesSum
     line = line.strip()
     firstPart, secondPart = line[:len(line)//2], line[len(line)//2:]
-    # print (firstPart, secondPart)
     sameLetters = list(set(firstPart) & set(secondPart))
     for l in sameLetters:
         prioritiesSum += (values[l])
 
 def main() -> None:
-    part_one()
+    #part_one()
+    part_two()
 
 if __name__ == "__main__":
     main()
